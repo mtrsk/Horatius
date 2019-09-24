12d3569705bf58d03e68930b6da03a97dc01e3f5
mtrsk <marcos.schonfinkel@protonmail.com>
2019-09-24 00:14:44 -0300

    Basic CLI

---
+++ src/CLI/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+open Verification
+
+[<EntryPoint>]
+let main argv =
+    let filepath1 = "matriculasSemDV.txt"
+    Validation.writeVDFile filepath1
+
+    let filepath2 = "matriculasParaVerificar.txt"
+    Validation.writeVerifiedFile filepath2
+    0 // return an integer exit code
